0c2d14471358dfee77105cc19683ade028f55cd6
SOHAM SARKAR <98482003+Sohoxic@users.noreply.github.com>
2022-10-01 18:32:21 +0530

    Create checker.js

---
+++ url-checker-files/jsImplementation/checker.js
@@ -0,0 +1,3 @@
+/*  	- An URL will be considered as VALID by these functions and can also check for invalid ( P.S: when true isn't always so :p )
+ *  	- A URL that is considered invalid for the API MUST be flagged as invalid by this tool 
+ *  .. in other words: this should be reflected in tests (not included within this file). */
